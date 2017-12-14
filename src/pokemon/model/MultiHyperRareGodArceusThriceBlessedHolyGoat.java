7fff08b6c515b3d7a5b9be63a16032a60af74ad0
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-12-14 13:55:57 -0700

    Added getters and setters using eclipse magic helping abilities, and other pictures and stuff.

---
+++ src/pokemon/model/MultiHyperRareGodArceusThriceBlessedHolyGoat.java
@@ -0,0 +1,9 @@
+package pokemon.model;
+
+public class MultiHyperRareGodArceusThriceBlessedHolyGoat extends Pokemon implements Ice, Fire
+{
+	public MultiHyperRareGodArceusThriceBlessedHolyGoat(int number, String name)
+	{
+		super(number, name);
+	}
+}
