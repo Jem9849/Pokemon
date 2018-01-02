f79ff3df6e8cb8a4861b952e59845809d223edbc
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-01-02 13:55:00 -0700

    Fixed controller and added code to panel to set it up.

--- src/pokemon/controller/PokemonController.java
+++ src/pokemon/controller/PokemonController.java
@@ -1,9 +1,51 @@
 package pokemon.controller;
 
+import pokemon.model.Pokemon;
+import java.util.List;
+import java.util.ArrayList;
+
 public class PokemonController
 {
-	public void start()
-	{
+	private List<Pokemon> pokedex;
+	
+		public List<Pokemon> getPokedex()
+		{	
+			return pokedex;
+		}
 		
-	}
+		public boolean isValidInteger(String input)
+		{
+			boolean valid = false;
+			
+			try
+			{
+				Integer.parseInt(input);
+				valid = true;
+			}
+			
+			catch(NumberFormatException error)
+			{
+				System.out.println("You need to input an int, " + input + " is not valid.");
+			}
+			
+			return valid;
+		}
+		
+		public boolean isValidDouble(String input)
+		{
+			boolean validation = false;
+			
+			try
+			{
+				Double.parseDouble(input);
+				validation = true;
+			}
+			
+			catch(NumberFormatException error)
+			{
+				System.out.println("You need to input a double, " + input + " is not valid.");
+			}
+			
+			return validation;
+		}
 }
--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -1,16 +1,8 @@
 package pokemon.view;
 
-import javax.swing.JPanel;
 import pokemon.controller.PokemonController;
 import java.awt.Color;
-import javax.swing.JButton;
-import javax.swing.SpringLayout;
-import javax.swing.JTextField;
-import javax.swing.JTextArea;
-import java.awt.Color;
-import javax.swing.JLabel;
-import javax.swing.JScrollPane;
-import javax.swing.ImageIcon;
+import javax.swing.*;
 import java.applet.Applet;
 import java.applet.AudioClip;
 import java.net.URL;
@@ -18,7 +10,109 @@ import java.net.URL;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener; 
 
-public class PokemonPanel
+public class PokemonPanel extends JPanel
 {
-
+	private PokemonController appController;
+	private SpringLayout appLayout;
+	
+	private JLabel healthLabel;
+	private JLabel attackLabel;
+	private JLabel nameLabel;
+	private JLabel numberLabel;
+	private JLabel evolvableLabel;
+	private JLabel modifierLabel;
+	private JLabel iconLabel;
+	
+	private JCheckBox evolvableBox;
+	private JTextField nameField;
+	private JTextField numberField;
+	private JTextField attackField;
+	private JTextField healthField;
+	private JTextField modifierField;
+	
+	private JTextArea descriptionArea;
+	private JTextArea typeArea;
+	
+	private JButton saveButton;
+	private JButton clearButton;
+	private JComboBox pokedexDropdown;
+	
+	private JPanel firstType;
+	private JPanel secondType;
+	private JPanel thirdType;
+	private JPanel fourthType;
+	
+	private void updatePokedexInfo(int index)
+	{
+		nameField.setText(appController.getPokedex().get(index).getName());
+		evolvableBox.setSelected(appController.getPokedex().get(index).isCanEvolve());
+		numberField.setText(appController.getPokedex().get(index).getNumber() + "");
+		attackField.setText(appController.getPokedex().get(index).getAttackPoints() + "");
+		healthField.setText(appController.getPokedex().get(index).getHealthPoints() + "");
+		modifierField.setText(appController.getPokedex().get(index).getEnhancementModifier() + "");
+	}
+	
+	public PokemonPanel(PokemonController appController)
+	{
+		super();
+		
+		this.appController = appController;
+		
+		healthLabel = new JLabel("5");
+		attackLabel = new JLabel("5");
+		nameLabel = new JLabel("5");
+		numberLabel = new JLabel("5");
+		evolvableLabel = new JLabel("5");
+		modifierLabel = new JLabel("5");
+		iconLabel = new JLabel("5");
+		descriptionArea = new JTextArea(10, 25);
+		typeArea = new JTextArea(10, 25);
+		saveButton = new JButton("Save");
+		clearButton = new JButton("Clear");
+		pokedexDropdown = new JComboBox();
+		firstType = new JPanel();
+		secondType = new JPanel();
+		thirdType = new JPanel();
+		fourthType = new JPanel();
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	public void setupPanel()
+	{
+		this.setLayout(appLayout);
+		this.add(healthLabel);
+		this.add(attackLabel);
+		this.add(nameLabel);
+		this.add(numberLabel);
+		this.add(evolvableLabel);
+		this.add(modifierLabel);
+		this.add(iconLabel);
+		this.add(saveButton);
+		this.add(clearButton);
+		this.add(nameField);
+		this.add(evolvableBox);
+		this.add(numberField);
+		this.add(attackField);
+		this.add(healthField);
+		this.add(modifierField);
+		
+		descriptionArea.setEnabled(false);
+		descriptionArea.setEditable(false);
+		typeArea.setEnabled(false);
+		typeArea.setEditable(false);
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+		
 }
