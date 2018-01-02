6b25d65d6d5d2d1a804cdaf5ba58d1cb601ffd21
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2018-01-02 12:00:22 -0700

    Added the frame and panel importation, and maybe some code that is likely to change.

--- src/pokemon/view/PokemonFrame.java
+++ src/pokemon/view/PokemonFrame.java
@@ -1,6 +1,35 @@
 package pokemon.view;
 
-public class PokemonFrame
-{
+import pokemon.controller.PokemonController;
+import javax.swing.JFrame;
 
+public class PokemonFrame extends JFrame
+{
+	private PokemonController appController; 
+	private PokemonPanel appPanel;
+	
+	public PokemonFrame(PokemonController appController)
+	{
+		super();
+		
+		this.appController = appController; 
+		//appPane = new PokemonPanel(appController);
+		
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		//this.setContentPane(appPane);
+		this.setTitle("POKEDEX");
+		this.setResizable(false);
+		this.setSize(1000, 500);
+		this.setVisible(true);
+	}
+	
+	public PokemonController getBaseController()
+	{
+		return appController; 
+	}
 }
--- src/pokemon/view/PokemonPanel.java
+++ src/pokemon/view/PokemonPanel.java
@@ -1,5 +1,23 @@
 package pokemon.view;
 
+import javax.swing.JPanel;
+import pokemon.controller.PokemonController;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.SpringLayout;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.ImageIcon;
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.net.URL;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener; 
+
 public class PokemonPanel
 {
 
